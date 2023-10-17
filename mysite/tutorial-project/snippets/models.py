7fa177c3b33097de2f20e9457691877a14431857
rajahwu <vmradford@gmail.com>
2023-10-17 02:00:45 -0400

    create snippet model

--- mysite/tutorial-project/snippets/models.py
+++ mysite/tutorial-project/snippets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TimeField()
+    linenos = models.BooleanField(default=False)
+    lanage = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    
+    class Meta:
+        ordering: ['created']
