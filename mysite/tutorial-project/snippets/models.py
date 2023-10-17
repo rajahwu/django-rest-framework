bb878f473f8fc3d067c0048a495df08cb472a152
rajahwu <vmradford@gmail.com>
2023-10-17 03:18:11 -0400

    create snippet model serializer

--- mysite/tutorial-project/snippets/models.py
+++ mysite/tutorial-project/snippets/models.py
@@ -6,12 +6,14 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
+
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
-    code = models.TimeField()
+    code = models.TextField()
     linenos = models.BooleanField(default=False)
-    lanage = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
-    
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
     class Meta:
-        ordering: ['created']
+        ordering = ['created']
